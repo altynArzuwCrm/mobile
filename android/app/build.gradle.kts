ab35badd3f153ce848713af6e63f8d21abfad99c
serdar <ibragimowserdar22@gmail.com>
2026-02-12 17:19:29 +0500

    1.0.3+1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.2+2"
+        versionCode = 3
+        versionName = "1.0.3+1"
     }
 
     signingConfigs {
